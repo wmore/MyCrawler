1985fda2a19c2a8aa5ce383bedf675fc65821713
wmore <ywgjwangyue@sina.com>
2016-04-19 21:44:08 +0700

    wyue

--- src/Spider.java
+++ src/Spider.java
@@ -62,4 +62,5 @@ public class Spider {
         }
         return results;
     }
+
 }
--- src/Zhihu.java
+++ src/Zhihu.java
@@ -1,10 +1,13 @@
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by wangyue on 16/4/19.
  */
 public class Zhihu {
     public String question;// the question title
+    public String questionDescription;// the description of question
     public String zhihuUrl;//  the page url
     public ArrayList<String> answers;// for store answers;
 
@@ -12,11 +15,24 @@ public class Zhihu {
     public Zhihu() {
         this.question = "";
         this.zhihuUrl = "";
+        this.questionDescription = "";
         this.answers = new ArrayList<String>();
     }
 
     @Override
     public String toString() {
-        return "QUESTION:"+this.question+"\tURL:"+this.zhihuUrl+"\tANSWERS:"+answers+"\n";
+        return "QUESTION:"+this.question+"\tDESCRIPTION:"+
+                "\tURL:"+this.zhihuUrl+"\tANSWERS:"+answers+"\n";
+    }
+
+    public boolean getRealUrl(String url){
+        Pattern pattern = Pattern.compile("question/(.*?)/");
+        Matcher matcher = pattern.matcher(url);
+        if(matcher.find()){
+            zhihuUrl = "http://www.zhihu.com/question/"+matcher.group(1);
+        }else{
+            return false;
+        }
+        return true;
     }
 }
